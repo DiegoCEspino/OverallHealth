0dd3664d3cf31e01a09d5d7b8ad99ae268ea48f8
daniel <dchellapan@my.bcit.ca>
2021-11-10 19:25:01 -0800

    added login page

---
+++ scripts/firebaseAPI_TEAM30.js
@@ -0,0 +1,27 @@
+
+ const firebaseConfig = {
+
+  apiKey: "AIzaSyC3zkYIjcG8Yo8mBkIjulzNqTZwtEvXE6A",
+
+  authDomain: "bby30-database.firebaseapp.com",
+
+  projectId: "bby30-database",
+
+  storageBucket: "bby30-database.appspot.com",
+
+  messagingSenderId: "218058933019",
+
+  appId: "1:218058933019:web:5347695cf52ddbf2e609be",
+
+  measurementId: "G-D8CS8HDE0K"
+
+};
+
+
+
+
+  // Initialize Firebase
+
+  const app = firebase.initializeApp(firebaseConfig);
+  const db = firebase.firestore();
+
