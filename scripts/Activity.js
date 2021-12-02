547c9e46c547d9d0454293db13bdea7a45889bd0
DiegoCEspino <diegoespino930@gmail.com>
2021-12-02 14:46:33 -0800

    Added descriptive comments to Activity.js

--- scripts/Activity.js
+++ scripts/Activity.js
@@ -1,3 +1,4 @@
+// Does the timer countdown and displays it on the page accordingly
 function timer(minutes, seconds, minutesShown, secondsShown) {
     if (seconds < 10) {
         secondsShown.innerHTML = "0" + seconds;
@@ -21,16 +22,19 @@ function timer(minutes, seconds, minutesShown, secondsShown) {
     }
 }
 
+// Reads the title and description of the activity that was selected and displays it on the page
 function activityDetails() {
+    // Activity title saved when the user clicked the activity box
     var title = localStorage.getItem("lastActivity");
+    // Sets the title
     document.getElementById("title").innerHTML = title;
-    db.collection('activities').get().then(snap => {
-        var size = snap.size
-        for (let i = 1; i <= size; i++) {
+    db.collection('activities').get().then(snap => { // Activities collection
+        var size = snap.size // Collection size
+        for (let i = 1; i <= size; i++) { // Loops through the activities
             db.collection("activities").doc(i.toString()).onSnapshot(
-                activity => {
-                    if (activity.data().name == title) {
-                        document.getElementById("description").innerHTML = activity.data().description;
+                activity => { // Activity pulled from ID
+                    if (activity.data().name == title) { // Finds the wanted activity document
+                        document.getElementById("description").innerHTML = activity.data().description; // Sets the activity description
                     }
                 }
             );
@@ -38,8 +42,10 @@ function activityDetails() {
     });
 }
 
+// Displays the activity details when loading the page
 activityDetails();
 
+// Sets the values for the timer when loading the page
 let minutesShown = document.getElementById("minutes");
 let secondsShown = document.getElementById("seconds");
 var seconds = localStorage.getItem("seconds");
@@ -67,30 +73,38 @@ else {
 
 var initialTime;
 
+// Runs the timer
 const time = async () => {
     initialTime = new Date();
     document.getElementById("start").remove();
     while (localStorage.getItem("minutes") > 0 || localStorage.getItem("seconds") > 0) {
-        await new Promise(r => setTimeout(r, 1000));
+        await new Promise(r => setTimeout(r, 1000)); // Waits a second
+        // Displays the new time
         timer(localStorage.getItem("minutes"), localStorage.getItem("seconds"), minutesShown, secondsShown);
     }
-    await new Promise(r => setTimeout(r, 1000));
-    secondsShown.innerHTML = "00";
+    await new Promise(r => setTimeout(r, 1000)); // Waits a second
+    secondsShown.innerHTML = "00"; // Sets the timer back to 00:00
+    // If the times reaches 00:00 and the timer was set by the user it send a notification / alarm
     if (localStorage.getItem("timerSet") == "true") {
         alert("Time's up!")
+        // Unsets the timer
         localStorage.setItem("timerSet", "false");
     }
 }
 
+// The page saves the time spent on the activity when the page closes
 window.onunload = function (event) {
-    var time = Math.abs(new Date() - initialTime);
+    var time = Math.abs(new Date() - initialTime); // Now - moment when the activity was started (Time that passed)
     //console.log(time);
-    if (!isNaN(time)) {
+    if (!isNaN(time)) { // If the timer started
+        // Saving the time to localStorage
         localStorage.setItem("activityTime", time);
     }
 };
 
+// Adds an event listener to the timer for it to be modifiable
 document.getElementById("timer").addEventListener("click", myFunction);
 function myFunction() {
+    // redirects to new page
     location.replace("setTimer.html");
 }
